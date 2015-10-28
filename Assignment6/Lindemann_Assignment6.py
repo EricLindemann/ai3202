6dc238ebd983dc4a4e372864007230126d04cbb7
EricLindemann <elindemann3@gmail.com>
2015-10-28 14:19:06 -0600

    deprecated conditional

--- Assignment6/Lindemann_Assignment6.py
+++ Assignment6/Lindemann_Assignment6.py
@@ -1,9 +1,5 @@
 import sys
 
-def conditional(myDict,using,listofAbb):
-    if len(listofAbb) == 1: #conditioned on one parameter 
-        if myDict[using][1] == None and myDict[listofAbb[0]][1] == None:
-            return myDict[using][0]
 
 
 
@@ -40,6 +36,54 @@ def marginal(myDict,using):
 
 
 
+def conditional(myDict,using,listofAbb):
+    print 'using = ', using, ' listofAbb =', listofAbb
+    if listofAbb == None or len(listofAbb) == 0: #calc marginal
+        return myDict[using][0]
+
+    if len(listofAbb) == 1: #conditioned on one parameter 
+        if myDict[using][1] == None and myDict[listofAbb[0]][1] == None: #S and P
+            return myDict[using][0]
+
+        elif using == 'S' or using == 'P' or using == '~P' or using == '~S':#deprecated
+            if listofAbb[0] == 'CPS' or listofAbb[0] == '~CPS' or  listofAbb[0] == 'C~PS' or listofAbb[0] == '~C~PS' or listofAbb[0] == 'CP~S' or  listofAbb[0] == '~CP~S' or listofAbb[0] == 'C~P~S' or listofAbb[0] == '~C~P~S': #S conditioned on CPS
+                tempList = []
+                tempList.append(using)
+                return condtional(myDict,listofAbb,tempList)*myDict[using][0] /myDict[listofAbb[0]][0]
+
+            elif listofAbb[0] == 'XC' or listofAbb[0] == 'X~C' or listofAbb[0] == '~XC' or listofAbb[0] == '~X~C' or listofAbb[0] == 'DC' or listofAbb[0] == '~DC' or   listofAbb[0] == 'D~C' or listofAbb[0] == '~D~C':
+                return condtional(myDict,using,'CPS')*condtional(myDict,'CPS',listofAbb) + conditional(myDict,using,'~CPS')*conditional(myDict,'~CPS',listofAbb)
+       
+        elif using == 'CPS' or using == '~CPS' or using == 'C~PS' or using  == '~C~PS' or using == 'CP~S' or using == '~CP~S' or using == 'C~P~S' or using == '~C~P~S': 
+            if listofAbb[0] == 'P' or listofAbb[0] == '~P':
+                list1 = listofAbb.append('S')
+                list2 = listofAbb.append('~S')
+                return conditional(myDict,using,list1)*myDict['S'][0] + conditional(myDict,using,list2)*myDict['~S'][0]
+
+            elif listofAbb[0] == 'S' or listofAbb[0] == '~S':
+                list1 = listofAbb.append('P')
+                list2 = listofAbb.append('~P')
+                return conditional(myDict,using,list1)*myDict['P'][0] + condtional(myDict,using,list2)*myDict['~P'][0]
+            
+            elif listofAbb[0] == 'XC' or listofAbb[0] == 'X~C' or listofAbb[0] == '~XC' or listofAbb[0] == '~X~C' or listofAbb[0] == 'DC' or listofAbb[0] == '~DC' or   listofAbb[0] == 'D~C' or listofAbb[0] == '~D~C':
+                tempList = []
+                tempList.append(using)
+                return conditional(myDict,listofAbb[0], tempList)*myDict[listofAbb[0]][0]/myDict[using][0]
+         
+        elif  using == 'XC' or using == 'X~C' or using == '~XC' or using == '~X~C' or using == 'DC' or using == '~DC' or using == 'D~C' or using == '~D~C':
+            if listofAbb[0] == 'CPS' or listofAbb[0] == '~CPS' or  listofAbb[0] == 'C~PS' or listofAbb[0] == '~C~PS' or listofAbb[0] == 'CP~S' or  listofAbb[0] == '~CP~S' or listofAbb[0] == 'C~P~S' or listofAbb[0] == '~C~P~S':         
+                return myDict[using][0]
+            else:
+                tempList = []
+                tempList.append('CPS')
+                tempNegatedList = []
+                tempNegatedList.append('~CPS')
+                print "listofAbb before entering is: ", listofAbb
+                return conditional(myDict,using,tempList)*conditional(myDict,'CPS',listofAbb) + conditional(myDict,using,tempNegatedList) * conditional(myDict,'~CPS',listofAbb) #same for S,P or D,X
+
+
+
+
 myDict = {}
 myDict['P'] = [.9,None]
 myDict['~P'] = [.1,None]
@@ -71,7 +115,7 @@ if probType == 'm': #marginal
     print marginal(myDict,sys.argv[3])
 elif probType == 'j': #joint
     print joint(myDict,sys.argv[3].split(","))
-elif probType == 'c': #conditional
+elif probType == 'g': #conditional
     conditionals = sys.argv[3].split(",")
     isFirst = True
     listofAbb = []
